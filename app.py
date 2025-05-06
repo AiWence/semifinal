92f9141f32dec182a41b63ac1e8d74847b4bd80c
AiWence <aranzado.airawence@cec.edu.ph>
2025-05-06 22:09:24 +0800

    second commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,3 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return 'Aira Wence Aranzado, BSIT III-A, System Integration and Architecture - semi-final exam'
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
